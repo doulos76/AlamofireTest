a80505c56b5e71f09843a0d48061db11679cd0bf
iOS_Dev <dave@iOS-Devs-MacBook-Pro.local>
2019-08-02 17:14:31 +0900

    add AlamofirePractice and .gitignore

---
+++ AlamofirePractice/AlamofirePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofirePractice
+//
+//  Created by iOS_Dev on 02/08/2019.
+//  Copyright © 2019 iOS_Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
